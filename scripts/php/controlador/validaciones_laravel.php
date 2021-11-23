bd3658c4339093bab7eb1390aecc2e7b973da0fe
Mario Marin Ramirez <mariomarin502t@gmail.com>
2021-11-23 00:16:16 -0600

    Se agregó las validaciones en Laravel

---
+++ scripts/php/controlador/validaciones_laravel.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Controllers; 
+
+class MensajesDeValidacion {
+    public function store(){
+       
+
+        
+
+    }
+
+}
+
+
+?>
--- scripts/php/vista/header.php
+++ scripts/php/vista/header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="shortcut icon" href="imagenes/Logo_Dreamhome.png" type="image/x-icon">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dreamhome Inicio</title>
 </head>
 <body>
     <h1>Entre alv</h1>
