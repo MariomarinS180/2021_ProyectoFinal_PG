001aa8dcba4c8dd2944acaae508f7336a61ac51d
Mario Marin Ramirez <mariomarin502t@gmail.com>
2021-11-24 17:43:18 -0600

    Se estableció la tabla

--- scripts/php/vista/header.php
+++ scripts/php/vista/header.php
@@ -16,43 +16,43 @@
     <div class="container">
         <div class="row">
             <div class="col-5">
-                    <div class="card">
-                        <div class="card-header">
-                            Departamentos
-                        </div>
-                        <div class="card-body">
-                            <form action="../controlador/procesar_abcc_dreamhome.php" method="POST">
-                                <div class="mb-3">
-                                    <label for="txtBranch" class="form-label">BranchNo</label>
-                                    <input type="text" class="form-control" name="txtBranch" id="txtBranch"  placeholder="4 Caracteres">
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="txtStreet" class="form-label">Street</label>
-                                    <input type="text" class="form-control" name="txtStreet" id="txtStreet"  placeholder="Nombre de la Calle">
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="txtCity" class="form-label">City</label>
-                                    <input type="text" class="form-control" name="txtCity" id="txtCity" placeholder="Nombe de la Ciudad">
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="txtpostCode" class="form-label">PostCode</label>
-                                    <input type="text" class="form-control" name="txtpostCode" id="txtpostCode"  placeholder="Codigo Postal">
-                                </div>
-
-                                <div class="btn-group" role="group" >
-                                    <button type="submit"  class="btn btn-success" onclick=" return validacionHeader()" >Agregar</button>
-                                    <button id="btnEditar" type="button" class="btn btn-warning">Editar</button>
-                                    <button id="btnCancelar" type="button" class="btn btn-primary">Cancelar</button>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="card-footer text-muted">
-                            Footer
-                        </div>
+                <div class="card">
+                    <div class="card-header">
+                        Departamentos
                     </div>
+                    <div class="card-body">
+                        <form action="../controlador/procesar_abcc_dreamhome.php" method="POST">
+                            <div class="mb-3">
+                                <label for="txtBranch" class="form-label">BranchNo</label>
+                                <input type="text" class="form-control" name="txtBranch" id="txtBranch" placeholder="4 Caracteres">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="txtStreet" class="form-label">Street</label>
+                                <input type="text" class="form-control" name="txtStreet" id="txtStreet" placeholder="Nombre de la Calle">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="txtCity" class="form-label">City</label>
+                                <input type="text" class="form-control" name="txtCity" id="txtCity" placeholder="Nombe de la Ciudad">
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="txtpostCode" class="form-label">PostCode</label>
+                                <input type="text" class="form-control" name="txtpostCode" id="txtpostCode" placeholder="Codigo Postal">
+                            </div>
+
+                            <div class="btn-group" role="group">
+                                <button type="submit" class="btn btn-success" onclick=" return validacionHeader()">Agregar</button>
+                                <button id="btnEditar" type="button" class="btn btn-warning">Editar</button>
+                                <button id="btnCancelar" type="button" class="btn btn-primary">Cancelar</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer text-muted">
+                        Footer
+                    </div>
+                </div>
             </div>
             <div class="col-7">
                 <table class="table table-striped table-bordered" id="tablaReg">
@@ -67,11 +67,25 @@
                     </thead>
                     <tbody>
                         <tr>
-                            <td>B1</td>
-                            <td>Gomez</td>
-                            <td>Zac</td>
-                            <td>99300</td>
-                            <td>Seleccionar | Borrar</td>
+                            <?php
+                            include('../modelo/dreamhomeDAO.php');
+                            $branchDao = new DreamhomeDAO();
+                            $res = $branchDao->mostrar();
+
+                            if (mysqli_num_rows($res) > 0) {
+                                //echo "<table class= 'table table-striped table-bordered'>";
+                                while ($fila = mysqli_fetch_assoc($res)) {
+                                    printf("<tr>
+                                         <td>" . $fila['branchNo'] . "</td>" .
+                                        "<td>" . $fila['street'] . "</td>" .
+                                        "<td>" . $fila['city'] . "</td>" .
+                                        "<td>" . $fila['postcode'] . "</td>" .
+                                        "<td> <a href='../modelo/dreamhomeDAO.php?branchNo=%s'> ELIMINAR</a> </td> </tr>", $fila['branchNo']);
+                                }
+                            } else {
+                                echo "SIN registros para mostrar";
+                            }
+                            ?>
                         </tr>
                         <tr>
                             <td scope="row"></td>
@@ -84,28 +98,7 @@
         </div>
     </div>
 
-    <?php
-    include('../modelo/dreamhomeDAO.php');
-    $branchDao = new DreamhomeDAO(); 
-    $res = $branchDao->mostrar(); 
 
-    if(mysqli_num_rows($res)>0){
-        echo "<table class=table table-striped table-bordered>";
-        while($fila = mysqli_fetch_assoc($res)){
-            printf("<tr>
-                        <td>".$fila['branchNo']."</td>".
-                        "<td>".$fila['street']."</td>".
-                        "<td>".$fila['city']."</td>".
-                        "<td>".$fila['postcode']."</td>".
-                        "<td> <a href='../modelo/dreamhomeDAO.php?branchNo=%s'> ELIMINAR</a> </td> </tr>", $fila['branchNo'] );
-        }
-
-    }else{
-        echo "SIN registros para mostrar";
-    }
-    echo "</table>";
-
-    ?>
 
 </body>
 
